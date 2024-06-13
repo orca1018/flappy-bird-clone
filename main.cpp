00288b981a22a8f3ab2558a9d3417b2b16c1e4cb
orca1018 <orca_1018@outlook.com>
2024-06-13 19:24:35 +0700

    loadAudio function, simplify code

--- main.cpp
+++ main.cpp
@@ -19,26 +19,7 @@ struct Audio {
   Sound wind;
 };
 
-struct Textures {
-  Texture flappy[3];
-  Texture base;
-  Texture gameover;
-  Texture message;
-  Texture background;
-  Texture pipe;
-};
-
-int main() {
-
-  sf::RenderWindow window(sf::VideoMode(288, 512), "Flappy Bird");
-  window.setFramerateLimit(120);
-  window.setKeyRepeatEnabled(false);
-  window.setVerticalSyncEnabled(true);
-
-  srand(time(0));
-
-  // load sounds to RAM
-  struct Audio sounds;
+void loadAudio(Audio &sounds) {
   sounds.die_buffer.loadFromFile("./audio/die.wav");
   sounds.die.setBuffer(sounds.die_buffer);
   sounds.hit_buffer.loadFromFile("./audio/hit.wav");
@@ -49,9 +30,31 @@ int main() {
   sounds.swoosh.setBuffer(sounds.swoosh_buffer);
   sounds.wind_buffer.loadFromFile("./audio/wind.wav");
   sounds.wind.setBuffer(sounds.wind_buffer);
+}
+
+struct Textures {
+  Texture flappy[3];
+  Texture base;
+  Texture gameover;
+  Texture message;
+  Texture background;
+  Texture pipe;
+};
+
+int main() {
+  RenderWindow window(VideoMode(288, 512), "Flappy Bird");
+  window.setFramerateLimit(120);
+  window.setKeyRepeatEnabled(false);
+  window.setVerticalSyncEnabled(true);
+
+  srand(time(0));
+
+  // load sounds to RAM
+  Audio sounds;
+  loadAudio(sounds);
 
   // load textures to RAM
-  struct Textures textures;
+  Textures textures;
   textures.flappy[0].loadFromFile("./sprites/yellowbird-upflap.png");
   textures.flappy[1].loadFromFile("./sprites/yellowbird-midflap.png");
   textures.flappy[2].loadFromFile("./sprites/yellowbird-downflap.png");
@@ -64,68 +67,65 @@ int main() {
     textures.flappy[0].loadFromFile("./sprites/bluebird-downflap.png");
     textures.flappy[1].loadFromFile("./sprites/bluebird-downflap.png");
     textures.flappy[2].loadFromFile("./sprites/bluebird-downflap.png");
-    */
+  */
   textures.base.loadFromFile("./sprites/base.png");
   textures.gameover.loadFromFile("./sprites/gameover.png");
   textures.message.loadFromFile("./sprites/message.png");
   textures.background.loadFromFile("./sprites/background-day.png");
-  // texture.background.loadFromFile("./sprites/background-night.png");
+  // textures.background.loadFromFile("./sprites/background-night.png");
   textures.pipe.loadFromFile("./sprites/pipe-green.png");
   // textures.pipe.loadFromFile("./sprites/pipe-red.png");
-};
 
-float x = 144.0f;
-float y = 256.0f;
+  // Set up bird sprite
+  Sprite birdSprite;
+  birdSprite.setTexture(textures.flappy[0]);
 
-// will calibrate these value later
-float velocity = 0.0f;
-const float gravity = 0.35f;
-const float flapStrength = -6.0f;
-const float maxVelocity = 15.0f; // Maximum falling speed
+  float x = 144.0f;
+  float y = 256.0f;
 
-// circle representing the bird
-sf::CircleShape circle(10);
-circle.setFillColor(sf::Color::Red);
-circle.setPosition(x, y);
+  // will calibrate these value later
+  float velocity = 0.0f;
+  const float gravity = 0.35f;
+  const float flapStrength = -6.0f;
+  const float maxVelocity = 15.0f; // Maximum falling speed
 
-while (window.isOpen()) {
-
-  sf::Event event;
-  while (window.pollEvent(event)) {
-    if (event.type == sf::Event::Closed) {
-      window.close();
+  while (window.isOpen()) {
+    Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == Event::Closed) {
+        window.close();
+      }
+      // Development phase shortcut to close the window
+      if (event.type == Event::KeyPressed && event.key.code == Keyboard::Q) {
+        window.close();
+      }
+      if (event.type == Event::KeyPressed &&
+          event.key.code == Keyboard::Space) {
+        velocity = flapStrength;
+        sounds.swoosh.play();
+      }
     }
-    // Development phase shortcut to close the window
-    if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Q) {
-      window.close();
+
+    velocity += gravity;
+    if (velocity > maxVelocity) {
+      velocity = maxVelocity;
     }
-    if (event.type == Event::KeyPressed &&
-        event.key.code == sf::Keyboard::Space) {
-      velocity = flapStrength;
-      sounds.swoosh.play();
+
+    y += velocity;
+
+    if (y < 0) {
+      y = 0;
+      velocity = 0;
+    } else if (y > window.getSize().y - birdSprite.getGlobalBounds().height) {
+      y = window.getSize().y - birdSprite.getGlobalBounds().height;
+      velocity = 0;
     }
+
+    window.clear();
+    birdSprite.setPosition(x, y);
+    window.draw(birdSprite);
+    window.display();
   }
 
-  velocity += gravity;
-  if (velocity > maxVelocity) {
-    velocity = maxVelocity;
-  }
-
-  y += velocity;
-
-  if (y < 0) {
-    y = 0;
-    velocity = 0;
-  } else if (y > window.getSize().y - circle.getRadius() * 2) {
-    y = window.getSize().y - circle.getRadius() * 2;
-    velocity = 0;
-  }
-
-  window.clear();
-  circle.setPosition(x, y);
-  window.draw(circle);
-  window.display();
-}
-
-return 0;
+  return 0;
 }
